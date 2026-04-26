afe4e8986667c89189cc65cf116850c3293a6f79
ROMERUU-dev <juvenal.romero@uabc.edu.mx>
2026-04-26 15:21:30 -0700

    Regenerate canonical GDS from corrected oscillator top cell

--- mag/export_gds_lef.tcl
+++ mag/export_gds_lef.tcl
@@ -1,9 +1,9 @@
 crashbackups stop
 drc off
 snap internal
-load tt_um_tetrahedral_oscilator -dereference
+load tt_um_tetrahedral_oscillator -dereference
 select top cell
 expand
-gds write ../gds/tt_um_tetrahedral_oscilator.gds
-lef write ../lef/tt_um_tetrahedral_oscilator.lef -pinonly
+gds write ../gds/tt_um_tetrahedral_oscillator.gds
+lef write ../lef/tt_um_tetrahedral_oscillator.lef -pinonly
 quit
